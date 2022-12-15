237d7d2bbc028390053b70f41cf10f98aa8b50c1
LivneRosenblum <livne@green-eye.ca>
2022-12-15 22:27:31 +0200

    adding decrypt functionality. remove comments and unnecessary code.

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -2,16 +2,22 @@ import { Router } from 'express';
 import { americanDeclaration } from '../data/declarationDoc';
 import { bealeCipher, plaintext } from '../data/bealeCipher';
 import { getEffectiveTypeParameterDeclarations } from 'typescript';
-
+const ILLEGAL_CHAR = -1;
 const encryptionHandler = Router();
 
-// encryptionHandler.get('/encrypt', (req, res, next) => {
-//   const plaintextAsCharacters = plaintext.split("");
-//   const americanDeclaration = americanDeclaration
-//   const cipher = plaintextAsCharacters.map((char) => {
-//    return americanDeclaration.indexOf
-//   })
-// });
+encryptionHandler.get('/encrypt', (req, res, next) => {
+  const plaintextAsCharacters = plaintext.split("").map(char => char.toLowerCase());
+  const americanDeclarationByWords = americanDeclaration.split(" ").map(word => word.toLowerCase());
+  const result = plaintextAsCharacters.map((char) => {
+    if (char.length == 1 && char.match(/[a-z]/i)) {
+      let index = americanDeclarationByWords.findIndex(word => word.startsWith(char));
+      return index + 1;
+    }
+    else{return ILLEGAL_CHAR}
+  });
+  const strResult = result.filter((index) => index != ILLEGAL_CHAR).join(', ');
+  res.json(strResult);
+});
 
 
 
